989d53538375241e4fb89cb61914d58a41583062
Leo <leochan2017@gmail.com>
2017-11-03 14:11:42 +0800

    增加BTC和ETH打赏渠道

--- app/pages/leo/leo.js
+++ app/pages/leo/leo.js
@@ -1,6 +1,8 @@
 const Util = require('../../utils/util.js');
 Page({
-    data: {},
+    data: {
+        canUseSetClipboardData: wx.canIUse('setClipboardData')
+    },
     payMeMoney() {
         wx.previewImage({
             urls: ['http://www.leojs.com/pay-me-money.jpg']
@@ -12,5 +14,51 @@ Page({
         });
     },
     // 定义转发
-    onShareAppMessage: Util.shareConfig
+    onShareAppMessage: Util.shareConfig,
+    payMeBtc() {
+        wx.setClipboardData({
+            data: '1PQg7wJNMVa3wZxtrMJiFmcHrQicsDtpsG',
+            success: res => {
+                wx.showModal({
+                    title: '感谢您的支持!',
+                    content: 'BTC钱包收款地址已经复制至您的手机剪切板',
+                    showCancel: false,
+                    confirmText: '好的'
+                });
+
+                wx.setStorage({
+                    key: 'isPayLeoMoney',
+                    data: true
+                });
+            },
+            fail: err => {
+                wx.showToast({
+                    title: '复制失败，请重试'
+                });
+            }
+        })
+    },
+    payMeEth() {
+        wx.setClipboardData({
+            data: '0xd7EAe8051D4309686c27D96cdE3C41A0ba91c43f',
+            success: res => {
+                wx.showModal({
+                    title: '感谢您的支持!',
+                    content: 'imtoken钱包收款地址已经复制至您的手机剪切板',
+                    showCancel: false,
+                    confirmText: '好的'
+                });
+
+                wx.setStorage({
+                    key: 'isPayLeoMoney',
+                    data: true
+                });
+            },
+            fail: err => {
+                wx.showToast({
+                    title: '复制失败，请重试'
+                });
+            }
+        })
+    }
 });
