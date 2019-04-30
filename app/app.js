3fddf0064341b4a2553409952d3cd9b81a883253
Leo <leochan2017@gmail.com>
2019-04-30 13:55:25 +0800

    1，首页增加分类入口；2，关于页去掉多余文字

--- app/app.js
+++ app/app.js
@@ -1,71 +1,72 @@
 App({
-    globalData: {
-        scene: -1,
-        // 是否大于204版本
-        isMore204: true
-    },
-    onLaunch(optitons) {
-        setTimeout(this.showAdJiLi, 800)
+  globalData: {
+    scene: -1,
+    // 是否大于204版本
+    isMore204: true
+  },
+  onLaunch(optitons) {
+    try {
+      const sysInfo = wx.getSystemInfoSync()
+      if (sysInfo.SDKVersion > '2.0.4') {
+        this.globalData.isMore204 = true
+      } else {
+        this.globalData.isMore204 = false
+      }
+    } catch (e) {}
 
-        this.showAdChaPin()
+    setTimeout(this.showAdJiLi, 800)
 
-        this.globalData.scene = optitons.scene
+    this.showAdChaPin()
 
-        try {
-            const sysInfo = wx.getSystemInfoSync()
-            if (sysInfo.SDKVersion > '2.0.4') {
-                this.globalData.isMore204 = true
-            } else {
-                this.globalData.isMore204 = false
-            }
-        } catch (e) {}
+    this.globalData.scene = optitons.scene
 
-        // this.initCopyCode()
-    },
-    /** 展示激励式广告 */
-    showAdJiLi() {
-        if (this.globalData.isMore204 && typeof wx.createRewardedVideoAd === 'function') {
-            let videoAd = wx.createRewardedVideoAd({
-                adUnitId: 'adunit-ff76dd10ecad5eba'
-            })
+    // this.initCopyCode()
+  },
+  /** 展示激励式广告 */
+  showAdJiLi() {
+    if (this.globalData.isMore204 && typeof wx.createRewardedVideoAd === 'function') {
+      let videoAd = wx.createRewardedVideoAd({
+        adUnitId: 'adunit-ff76dd10ecad5eba'
+      })
 
-            if (typeof videoAd.load === 'function') {
-                videoAd.load()
-                    .then(() => videoAd.show())
-                    .catch(err => console.log(err.errMsg))
-            }
-        }
-    },
-    /** 展示插屏广告 */
-    showAdChaPin() {
-        if (wx.createInterstitialAd) {
-            let intersitialAd = wx.createInterstitialAd({
-                adUnitId: 'adunit-9bedc6aec11d47c0'
-            })
-
-            intersitialAd.show().catch(err => console.log(err.errMsg))
-        }
+      if (typeof videoAd.load === 'function') {
+        videoAd
+          .load()
+          .then(() => videoAd.show())
+          .catch(err => console.log(err.errMsg))
+      }
     }
-    /** 复制神秘代码 */
-    // initCopyCode() {
-    // const d = '睿禾l倩宸唯y茗秀静3祥虹'
+  },
+  /** 展示插屏广告 */
+  showAdChaPin() {
+    if (wx.createInterstitialAd) {
+      let intersitialAd = wx.createInterstitialAd({
+        adUnitId: 'adunit-9bedc6aec11d47c0'
+      })
 
-    // function _setData() {
-    //     wx.setClipboardData({
-    //         data: d,
-    //         success: _ => {
-    //             wx.hideLoading()
-    //         }
-    //     })
-    // }
+      intersitialAd.show().catch(err => console.log(err.errMsg))
+    }
+  }
+  /** 复制神秘代码 */
+  // initCopyCode() {
+  // const d = '睿禾l倩宸唯y茗秀静3祥虹'
 
-    // let tGet = setInterval(_ => {
-    //     wx.getClipboardData({
-    //         success: res => {
-    //             if (res.data === d) return clearInterval(tGet)
-    //             _setData()
-    //         }
-    //     })
-    // }, 2000)
-    // }
+  // function _setData() {
+  //     wx.setClipboardData({
+  //         data: d,
+  //         success: _ => {
+  //             wx.hideLoading()
+  //         }
+  //     })
+  // }
+
+  // let tGet = setInterval(_ => {
+  //     wx.getClipboardData({
+  //         success: res => {
+  //             if (res.data === d) return clearInterval(tGet)
+  //             _setData()
+  //         }
+  //     })
+  // }, 2000)
+  // }
 })
