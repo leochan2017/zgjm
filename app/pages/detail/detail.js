1d85c36df42add680c904d4d92645fd7ffdc1c35
Leo <leochan2017@gmail.com>
2019-05-22 10:16:30 +0800

    修复线上广告不展示的 bug？

--- app/pages/detail/detail.js
+++ app/pages/detail/detail.js
@@ -2,88 +2,92 @@ const datas = require('../../utils/data.js')
 const Util = require('../../utils/util.js')
 // const APP = getApp()
 Page({
-    data: {
-        fromTitleList: false,
-        fromShare: false,
-        isLoading: true,
-        searchKey: '', // 输入查询的关键字
-        results: [], // 找寻到的列表
-        datas: datas.dreamsData,
-        minWrapHeight: 900
-    },
-    // 查询数据库
-    getData() {
-        let arr = []
-        const that = this
-        const key = that.data.searchKey
-        const datas = that.data.datas
+  data: {
+    fromTitleList: false,
+    fromShare: false,
+    isLoading: true,
+    searchKey: '', // 输入查询的关键字
+    results: [], // 找寻到的列表
+    datas: datas.dreamsData,
+    minWrapHeight: 900
+  },
+  // 查询数据库
+  getData() {
+    let arr = []
+    const that = this
+    const key = that.data.searchKey
+    const datas = that.data.datas
 
-        wx.showLoading({
-            title: '正在查询中',
-            mask: true
-        })
+    wx.showLoading({
+      title: '正在查询中',
+      mask: true
+    })
 
-        for (let i in datas) {
-            let item = datas[i]
-            if (item.title.indexOf(key) != -1) {
-                arr.push(item)
-            }
-        }
-
-        that.setData({
-            isLoading: false,
-            results: arr
-        })
-
-        setTimeout(() => {
-            wx.hideLoading()
-        }, 300)
-    },
-    // 自动计算撑开页面的内容区高度
-    resetWrapMinHeight: function () {
-        wx.getSystemInfo({
-            success: res => {
-                const windowHeight = res.windowHeight
-                // console.log('windowHeight', windowHeight)
-                if (windowHeight) {
-                    let minWrapHeight = windowHeight - 190
-                    if (this.data.fromShare) minWrapHeight = minWrapHeight - 65
-                    // console.log('minWrapHeight', minWrapHeight)
-                    this.setData({
-                        minWrapHeight
-                    })
-                }
-            }
-        })
-    },
-    // 定义转发
-    onShareAppMessage: function () {
-        // 如果没有查到任何内容则分享首页吧
-        if (this.data.results.length == 0) return Util.shareConfig()
-
-        const key = this.data.searchKey
-        const title = `梦到"${key}"原来是预示着...`
-
-        return {
-            title: title,
-            path: `/pages/detail/detail?key=${key}&fromShare=true`
-        }
-    },
-    onLoad: function (option) {
-        this.setData({
-            searchKey: option.key || '大风'
-        })
-
-        option.fromTitleList && this.setData({
-            fromTitleList: true
-        })
-
-        option.fromShare && this.setData({
-            fromShare: true
-        })
-
-        this.resetWrapMinHeight()
-
-        this.getData()
+    for (let i in datas) {
+      let item = datas[i]
+      if (item.title.indexOf(key) != -1) {
+        arr.push(item)
+      }
     }
+
+    that.setData({
+      isLoading: false,
+      results: arr
+    })
+
+    setTimeout(() => {
+      wx.hideLoading()
+    }, 300)
+  },
+  // 自动计算撑开页面的内容区高度
+  resetWrapMinHeight: function() {
+    wx.getSystemInfo({
+      success: res => {
+        const windowHeight = res.windowHeight
+        // console.log('windowHeight', windowHeight)
+        if (windowHeight) {
+          let minWrapHeight = windowHeight - 190
+          if (this.data.fromShare) minWrapHeight = minWrapHeight - 65
+          // console.log('minWrapHeight', minWrapHeight)
+          this.setData({
+            minWrapHeight
+          })
+        }
+      }
+    })
+  },
+  // 定义转发
+  onShareAppMessage: function() {
+    // 如果没有查到任何内容则分享首页吧
+    if (this.data.results.length == 0) return Util.shareConfig()
+
+    const key = this.data.searchKey
+    const title = `梦到"${key}"原来是预示着...`
+
+    return {
+      title: title,
+      path: `/pages/detail/detail?key=${key}&fromShare=true`
+    }
+  },
+  onLoad: function(option) {
+    this.setData({
+      searchKey: option.key || '大风'
+    })
+
+    option.fromTitleList &&
+      this.setData({
+        fromTitleList: true
+      })
+
+    option.fromShare &&
+      this.setData({
+        fromShare: true
+      })
+
+    this.resetWrapMinHeight()
+
+    this.getData()
+
+    Util.showAdJiLi()
+  }
 })
--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -1,51 +1,49 @@
 const Util = require('../../utils/util.js')
 Page({
-    data: {
-        searchKey: '' // 输入查询的关键字
-    },
-    // 检查输入内容
-    checkData() {
-        const key = this.data.searchKey
-        if (key == '') {
-            wx.showModal({
-                content: '搜索关键词为空',
-                showCancel: false,
-                confirmText: '我知道了',
-                confirmColor: '#888'
-            })
-            return false
-        }
-        return true
-    },
-    // 执行搜索
-    searchTap() {
-        if (!this.checkData()) return
+  data: {
+    searchKey: '' // 输入查询的关键字
+  },
+  // 检查输入内容
+  checkData() {
+    const key = this.data.searchKey
+    if (key == '') {
+      wx.showModal({
+        content: '搜索关键词为空',
+        showCancel: false,
+        confirmText: '我知道了',
+        confirmColor: '#888'
+      })
+      return false
+    }
+    return true
+  },
+  // 执行搜索
+  searchTap() {
+    if (!this.checkData()) return
 
-        Util.showAdJiLi()
+    let key = this.data.searchKey
 
-        let key = this.data.searchKey
+    const index1 = key.indexOf('梦到了')
+    if (index1 != -1) key = key.substr(index1 + 3)
 
-        const index1 = key.indexOf('梦到了')
-        if (index1 != -1) key = key.substr(index1 + 3)
+    const index2 = key.indexOf('梦到')
+    if (index2 != -1) key = key.substr(index2 + 2)
 
-        const index2 = key.indexOf('梦到')
-        if (index2 != -1) key = key.substr(index2 + 2)
+    const index3 = key.indexOf('梦见了')
+    if (index3 != -1) key = key.substr(index3 + 3)
 
-        const index3 = key.indexOf('梦见了')
-        if (index3 != -1) key = key.substr(index3 + 3)
+    const index4 = key.indexOf('梦见')
+    if (index4 != -1) key = key.substr(index4 + 2)
 
-        const index4 = key.indexOf('梦见')
-        if (index4 != -1) key = key.substr(index4 + 2)
-
-        wx.navigateTo({
-            url: '../detail/detail?key=' + key
-        })
-    },
-    // 搜索内容变动
-    changeSearchKey(e) {
-        const searchKey = e.detail.value
-        this.setData({ searchKey })
-    },
-    // 定义转发
-    onShareAppMessage: Util.shareConfig
+    wx.navigateTo({
+      url: '../detail/detail?key=' + key
+    })
+  },
+  // 搜索内容变动
+  changeSearchKey(e) {
+    const searchKey = e.detail.value
+    this.setData({ searchKey })
+  },
+  // 定义转发
+  onShareAppMessage: Util.shareConfig
 })
